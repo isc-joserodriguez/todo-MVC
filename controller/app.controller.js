3a71859dee90b02b8054bccc454844608850a672
José Rodriguez <joanrodriguezhe@ittepic.edu.mx>
2020-01-02 12:01:05 -0700

    Finish

--- controller/app.controller.js
+++ controller/app.controller.js
@@ -1,6 +1,101 @@
 import { Todo } from '../models/Todo.model.js'
-var model = new Todo();
 $(document).ready(function () {
-    console.log('corriendo');
+    var model = new Todo();
+
+    var selecc = 1;
+
+    //DOM Elements
+    var todoList = $('#todoList');
+    var inputTodo = $('#inputTodo');
+    var btnAll = $('#btnAll');
+    var btnCompleted = $('#btnCompleted');
+    var btnUncompleted = $('#btnUncompleted');
+
+    //DOM Dyn
+    var btnEdit = '.btnEdit';
+    var btnDel = '.btnDel';
+
+    function updateList() {
+        todoList.empty();
+        switch (selecc) {
+            case 1:
+                model = new Todo();
+                break;
+            case 2:
+                model.completed();
+                break;
+            case 3:
+                model.uncompleted();
+                break
+        }
+        model.todos.forEach(todo => {
+            todoList.append(
+                '<li id="taskLi' + todo.id + '" class="custom-checkbox mb-2 d-flex align-items-center justify-content-between task-' + todo.status + '">' +
+                '<input type="checkbox" ' + ((todo.status) ? 'checked' : '') + ' class="custom-control-input check-task" id="task' + todo.id + '" name="task' + todo.id + '">' +
+                '<label id="label' + todo.id + '" class="custom-control-label" for="task' + todo.id + '">' + todo.name + '</label>' +
+                '<button type="button" class="ml-2 btn btn-sm btn-outline-info" data-toggle="collapse" href="#info' + todo.id + '" role="button" aria-expanded="false" aria-controls="info' + todo.id + '"><i class="fas fa-info"></i></button>' +
+                '</li>' +
+                '<div class="collapse" id="info' + todo.id + '">' +
+                '<div class="card card-body bg-dark">' +
+                '<div class="row">' +
+                '<div class="col-8">' +
+                '<input type="text" class="form-control edit-task" id="edit' + todo.id + '" value="' + todo.name + '">' +
+                '</div>' +
+                '<div class="col-2 d-flex align-item-center">' +
+                '<button type="button" class="btnEdit ml-2 btn btn-sm btn-outline-info"><i class="fas fa-pen"></i></button>' +
+                '<button type="button" class="btnDel ml-2 btn btn-sm btn-outline-danger"><i class="fas fa-trash-alt"></i></button>' +
+                '</div>' +
+                '</div>' +
+                '<br>' +
+                '<span class="date text-right">' + ((todo.status) ? 'Completed: ' : 'Created: ') + todo.dateCreated + '</span>' +
+                '</div>' +
+                '</div>');
+        });
+    }
+    updateList();
+
+    //Events
+    inputTodo.on('keypress', function (e) {
+        if (e.keyCode === 13 && inputTodo.val() !== '') {
+            model.addTodo(inputTodo.val());
+            inputTodo.val('');
+            updateList();
+        }
+    });
+    $(document).on('click', btnEdit, function () {
+        var id = $(this).parent().parent().parent().parent().attr('id').substring(4);
+        model.editTodo(parseInt(id), $('#edit' + id).val());
+        $('#label' + id).text($('#edit' + id).val());
+        updateList();
+    });
+    $(document).on('click', btnDel, function () {
+        var id = $(this).parent().parent().parent().parent().attr('id').substring(4);
+        model.deleteTodo(parseInt(id));
+        updateList();
+    });
+    $(document).on('change', '.check-task', function() {
+        var target=$(this);
+        var id=target.attr('id').substring(4);
+        if(target.prop( "checked")){
+            target.parent().removeClass('task-false');
+            target.parent().addClass('task-true');
+        }else{
+            target.parent().removeClass('task-true');
+            target.parent().addClass('task-false');
+        }
+        model.statusTodo(parseInt(id));
+    });
+    //Mostrar tareas
+    btnAll.on('click', function () {
+        selecc = 1;
+        updateList();
+    });
+    btnCompleted.on('click', function () {
+        selecc = 2;
+        updateList();
+    });
+    btnUncompleted.on('click', function () {
+        selecc = 3;
+        updateList();
+    });
 });
-console.log('ccc');
--- models/Todo.model.js
+++ models/Todo.model.js
@@ -2,17 +2,77 @@ export class Todo {
     constructor() {
         var todo = localStorage.getItem('todos');
         this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
-        console.log(this.todos)
     }
 
     addTodo(name) {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
         const newTodo = {
             id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
             name: name,
-            dateCreated: new Date(),
-            dateFinished: new Date(),
-            status: false,
+            dateCreated: formatAMPM(),
+            dateFinished: formatAMPM(),
+            status: true,
         }
         this.todos.push(newTodo);
+        localStorage.setItem('todos', JSON.stringify(this.todos));
     }
+
+    editTodo(id, updatedName) {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
+        this.todos = this.todos.map(todo =>
+            todo.id === id ?
+                {
+                    id: todo.id,
+                    name: updatedName,
+                    dateCreated: todo.dateCreated,
+                    dateFinished: todo.dateFinished,
+                    status: todo.status
+                } : todo
+        );
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+
+    deleteTodo(id) {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
+        this.todos = this.todos.filter(todo => todo.id !== id);
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+
+    statusTodo(id) {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
+        this.todos = this.todos.map(todo =>
+            todo.id === id ?
+                {
+                    id: todo.id,
+                    name: todo.name,
+                    dateCreated: todo.dateCreated,
+                    dateFinished: formatAMPM(),
+                    status: !todo.status
+                } : todo
+        );
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+    completed() {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
+        this.todos = this.todos.filter(todo => todo.status);
+    }
+    uncompleted() {
+        var todo = localStorage.getItem('todos');
+        this.todos = JSON.parse(todo) ? JSON.parse(todo) : [];
+        this.todos = this.todos.filter(todo => !todo.status);
+    }
+}
+function formatAMPM() {
+    var d = new Date(),
+        minutes = d.getMinutes().toString().length == 1 ? '0' + d.getMinutes() : d.getMinutes(),
+        hours = d.getHours().toString().length == 1 ? '0' + d.getHours() : d.getHours(),
+        ampm = d.getHours() >= 12 ? 'pm' : 'am',
+        months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[d.getDay()] + ' ' + months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() + ' ' + hours + ':' + minutes + ampm;
 }
